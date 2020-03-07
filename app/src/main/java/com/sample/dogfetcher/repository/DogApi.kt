77d3dee5c16dbd8310db57495cce9cefe4d5fe4b
Kevin Campos <4038983+kevinmcampos@users.noreply.github.com>
2020-03-07 18:31:09 +0000

    Just the Retrofit Api as Coroutines

--- app/src/main/java/com/sample/dogfetcher/repository/DogApi.kt
+++ app/src/main/java/com/sample/dogfetcher/repository/DogApi.kt
@@ -1,11 +1,10 @@
 package com.sample.dogfetcher.repository
 
-import io.reactivex.Single
 import retrofit2.http.GET
 
 interface DogApi {
 
     @GET("/api/breeds/image/random")
-    fun getRandomDogImage(): Single<DogResponse>
+    suspend fun getRandomDogImage(): DogResponse
 
 }
--- app/src/main/java/com/sample/dogfetcher/repository/DogRepository.kt
+++ app/src/main/java/com/sample/dogfetcher/repository/DogRepository.kt
@@ -1,13 +1,16 @@
 package com.sample.dogfetcher.repository
 
 import io.reactivex.Single
+import kotlinx.coroutines.rx2.rxSingle
 
 class DogRepository constructor(
     private val dogApi: DogApi
 ) {
 
     fun getRandomDogImage(): Single<DogResponse> {
-        return dogApi.getRandomDogImage()
+        return rxSingle {
+            dogApi.getRandomDogImage()
+        }
     }
 
 }
