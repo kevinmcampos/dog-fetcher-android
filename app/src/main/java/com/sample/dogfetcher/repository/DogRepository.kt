545b19fa554b2cba2168bdb86598859a74ad7b32
Kevin Campos <4038983+kevinmcampos@users.noreply.github.com>
2020-03-07 18:39:13 +0000

    Just the repository as Coroutines

--- app/src/main/java/com/sample/dogfetcher/repository/DogRepository.kt
+++ app/src/main/java/com/sample/dogfetcher/repository/DogRepository.kt
@@ -1,13 +1,13 @@
 package com.sample.dogfetcher.repository
 
-import io.reactivex.Single
+import kotlinx.coroutines.rx2.await
 
 class DogRepository constructor(
     private val dogApi: DogApi
 ) {
 
-    fun getRandomDogImage(): Single<DogResponse> {
-        return dogApi.getRandomDogImage()
+    suspend fun getRandomDogImage(): DogResponse {
+        return dogApi.getRandomDogImage().await()
     }
 
 }
--- app/src/main/java/com/sample/dogfetcher/usecase/FetchDogUseCase.kt
+++ app/src/main/java/com/sample/dogfetcher/usecase/FetchDogUseCase.kt
@@ -3,6 +3,7 @@ package com.sample.dogfetcher.usecase
 import com.sample.dogfetcher.model.DogInfo
 import com.sample.dogfetcher.repository.DogRepository
 import io.reactivex.Single
+import kotlinx.coroutines.rx2.rxSingle
 
 
 class FetchDogUseCase(
@@ -11,7 +12,7 @@ class FetchDogUseCase(
 ) {
 
     operator fun invoke(): Single<DogInfo> {
-        return dogRepository.getRandomDogImage()
+        return rxSingle { dogRepository.getRandomDogImage() }
             .flatMap { dogResponse ->
                 val dogImageUrl = dogResponse.message
                 val breeds = classifyDogUseCase.invoke(dogImageUrl)
