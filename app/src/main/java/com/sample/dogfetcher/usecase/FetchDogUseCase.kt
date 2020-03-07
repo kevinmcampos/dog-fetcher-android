199cbd1e31bf7e1ac41c5e3f7ffa4d175bc6aa03
Kevin Campos <4038983+kevinmcampos@users.noreply.github.com>
2020-03-07 18:43:01 +0000

    Just the use case as Coroutines without changing the caller

--- app/src/main/java/com/sample/dogfetcher/usecase/FetchDogUseCase.kt
+++ app/src/main/java/com/sample/dogfetcher/usecase/FetchDogUseCase.kt
@@ -3,6 +3,8 @@ package com.sample.dogfetcher.usecase
 import com.sample.dogfetcher.model.DogInfo
 import com.sample.dogfetcher.repository.DogRepository
 import io.reactivex.Single
+import kotlinx.coroutines.rx2.await
+import kotlinx.coroutines.rx2.rxSingle
 
 
 class FetchDogUseCase(
@@ -11,14 +13,13 @@ class FetchDogUseCase(
 ) {
 
     operator fun invoke(): Single<DogInfo> {
-        return dogRepository.getRandomDogImage()
-            .flatMap { dogResponse ->
-                val dogImageUrl = dogResponse.message
-                val breeds = classifyDogUseCase.invoke(dogImageUrl)
+        return rxSingle {
+            val dogResponse = dogRepository.getRandomDogImage().await()
+            val dogImageUrl = dogResponse.message
+            val breeds = classifyDogUseCase.invoke(dogImageUrl)
 
-                Single.just(DogInfo(dogImageUrl = dogImageUrl, breedRecognitionList = breeds))
-            }
-
+            DogInfo(dogImageUrl = dogImageUrl, breedRecognitionList = breeds)
+        }
     }
 
 }
